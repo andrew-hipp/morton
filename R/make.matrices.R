2958f0c80f3d77d2220317b5a556db226f0ae658
andrew-hipp <andrew-hipp@users.noreply.github.com>
2016-09-06 05:27:15 -0500

    Create make.matrices.R
    added for back-compatibility with source file on Dryad for Carex NCBI paper (http://www.bioone.org/doi/abs/10.1600/036364416X692505)

---
+++ R/make.matrices.R
@@ -0,0 +1,9 @@
+## Created 2016-09-06 for back-compatibility with Syst Bot 2016 (NCBI) paper
+source('https://raw.githubusercontent.com/andrew-hipp/morton/R/make.shared.gene.matrix.R')
+source('https://raw.githubusercontent.com/andrew-hipp/morton/R/orphan.loci.R')
+source('https://raw.githubusercontent.com/andrew-hipp/morton/R/orphan.individuals.R')
+source('https://raw.githubusercontent.com/andrew-hipp/morton/R/make.fasta.files.R')
+source('https://raw.githubusercontent.com/andrew-hipp/morton/R/refine.fasta.R')
+source('https://raw.githubusercontent.com/andrew-hipp/morton/R/make.unique.vouchers.R')
+source('https://raw.githubusercontent.com/andrew-hipp/morton/R/make.gene.matrix.R')
+source('https://raw.githubusercontent.com/andrew-hipp/morton/R/gene.matrix.stats.R')
