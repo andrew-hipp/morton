0654f6921046ce7085ad0d69492c3f563d5222dd
Andrew Hipp <andrew-hipp@users.noreply.github.com>
2024-02-22 15:43:06 -0600

    Update label.elements.R
    Return elements defaults to '|'

--- R/label.elements.R
+++ R/label.elements.R
@@ -1,5 +1,5 @@
 label.elements <-
-function(x, delim = '|', returnNum = 1, returnDelim = ' ', ...) {
+function(x, delim = '|', returnNum = 1, returnDelim = '|', ...) {
 ## finds any label at the tips; default assumes pipe delimitation, and the element of interest is b/f the first pipe
   if('phylo' %in% class(x)) labelVector <- x$tip.label
   else labelVector <- x
